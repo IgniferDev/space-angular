b35710727eda1132a1f518c13d3f19953dace22c
Senn <GersonEM8900@outlook.com>
2025-11-16 22:34:55 -0600

    Modificaciones de Paginas de Proposito y Nosotros
    Se modificaron y amplio la información sobre las paginas de proposito y nosotros asi coo añadir imagenes de los integrantes del equipo

--- src/app/web-page/pages/purpose/purpose.component.ts
+++ src/app/web-page/pages/purpose/purpose.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { NavegationMenuComponent } from '../../components/navegation-menu/navegation-menu.component';
 
 @Component({
   selector: 'app-purpose-page',
   standalone: true,
-  imports: [CommonModule, NavegationMenuComponent],
+  imports: [CommonModule, NavegationMenuComponent,RouterModule],
   templateUrl: './purpose.component.html',
   styleUrls: ['./purpose.component.css']
 })
