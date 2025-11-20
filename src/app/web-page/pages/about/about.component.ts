680d6ffbe92405abd2138552208d241a05667121
Senn <GersonEM8900@outlook.com>
2025-11-20 14:38:07 -0600

    Rediseño de Nosotros
    Se rediseño completamente la pagina de About Us para una mejor interacción con el usuario e información más clara de los integrantes del proyecto

--- src/app/web-page/pages/about/about.component.ts
+++ src/app/web-page/pages/about/about.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { NavegationMenuComponent } from '../../components/navegation-menu/navegation-menu.component';
 
 @Component({
   selector: 'app-about-page',
   standalone: true,
-  imports: [CommonModule, NavegationMenuComponent],
+  imports: [CommonModule, NavegationMenuComponent, RouterLink],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
